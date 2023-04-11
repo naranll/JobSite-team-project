487295a0c0719101c185f307b45fdac95fadfd74
Morning <morning@admins-iMac-3.local>
2023-04-11 12:08:39 +0800

    JobPage

--- FE/src/pages/index.tsx
+++ FE/src/pages/index.tsx
@@ -1,22 +1,31 @@
 import JobCard from "@/components/JobCard";
 import { JobType } from "@/util/Types";
 import styles from "../styles/Main.module.css";
+// import Layout from "@/components/Layout";
+import Link from "next/link";
 
 export default function Home(props: { jobs: JobType[] }): JSX.Element {
   const { jobs } = props;
   return (
+    // <Layout>
     <div className={styles.main}>
       <h1>JOB BOARD</h1>
       <div>
         <input placeholder="search" />
         <button>search</button>
       </div>
-      {jobs.map((job: JobType, index: number) => (
-        <div key={index}>
-          <JobCard {...job} />
-        </div>
-      ))}
+      {jobs.map(
+        (job: JobType, index: number): JSX.Element => (
+          <div key={index}>
+            <Link href={`jobs/${job._id}`}>
+              <JobCard {...job} />
+            </Link>
+            {/* <JobCard {...job} /> */}
+          </div>
+        )
+      )}
     </div>
+    // </Layout>
   );
 }
 
