9eaa1ef4d7a25ba5a8f450cadba92c82708809d0
Morning <morning@1-9.local>
2023-04-14 11:22:49 +0800

    initialized dashboard be

---
+++ AD/dash-be/controller/category-controller.ts
@@ -0,0 +1,7 @@
+import Category from "../model/Category";
+import express from "express";
+
+const categoryRouter = express.Router();
+
+console.log("job router connected");
+export default categoryRouter;
