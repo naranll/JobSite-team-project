436082537e299fe2d5bf31da866061485fbc2f98
Naraa <naraa_togy@yahoo.com>
2023-04-16 17:37:32 +0800

    added svg logos

--- AD/dash-fe/src/components/SideMenu.tsx
+++ AD/dash-fe/src/components/SideMenu.tsx
@@ -1,25 +1,26 @@
 import Link from "next/link";
-import LogoDashboard from "./icons/LogoDashboard";
+import * as Logos from "./icons/Logos";
 
 export default function SideMenu(): JSX.Element {
-  const navlinkStyle = "text-2xl font-bold mb-4";
+  const navlinkStyle = "flex items-center gap-[15px] text-2xl font-bold mb-4";
   return (
     <div className="max-w-[287px] w-full h-screen py-[56px] text-white bg-gradient-to-b from-[#9F69B8] to-[#4D8BCC]"> 
-      <h1 className="w-[227px] m-auto mb-[52px] flex items-center gap-[13px] text-3xl font-black"><LogoDashboard/> Dashboard</h1>
-      <ul className="w-[227px] h-[500px] m-auto border-solid border-2 border-black">
-        <Link
-          href="/"
-        >
-          <li className={navlinkStyle}>Jobs</li>
+      <h1 className="w-[227px] m-auto mb-[52px] flex items-center gap-[13px] text-3xl font-black"><Logos.MenuLogo/> Dashboard</h1>
+      <ul className="w-[227px] h-[500px] m-auto">
+        <Link href="/">
+          <li className={navlinkStyle}><Logos.HomeLogo/> Home</li>
         </Link>
         <Link href="/">
-          <li className={navlinkStyle}>Users</li>
+          <li className={navlinkStyle}><Logos.JobLogo/>Jobs</li>
+        </Link>
+        <Link href="/">
+          <li className={navlinkStyle}><Logos.UserLogo/>Users</li>
         </Link>
         <Link href="/" >
-          <li className={navlinkStyle}>Applications</li>
+          <li className={navlinkStyle}><Logos.ApplicationLogo/>Applications</li>
         </Link>
         <Link href="/">
-          <li className={navlinkStyle}>Admin</li>
+          <li className={navlinkStyle}><Logos.AdminLogo/>Admin</li>
         </Link>
       </ul>
     </div>
--- AD/dash-fe/src/components/icons/MenuLogo.tsx
+++ AD/dash-fe/src/components/icons/MenuLogo.tsx
@@ -1,4 +1,4 @@
-export default function LogoDashboard():JSX.Element{
+export default function MenuLogo():JSX.Element{
     return <svg width="39" height="47" viewBox="0 0 39 47" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect x="2" y="4" width="36" height="42" rx="8" fill="white"/>
     <line x1="17.9999" y1="46.0056" x2="17.9999" y2="0.994446" stroke="#976DBA" strokeWidth="3"/>
