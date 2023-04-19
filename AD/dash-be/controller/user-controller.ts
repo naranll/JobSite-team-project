13594a8e13290a0fce6d7c6d568da2a11596ff3f
Morning <morning@1-10.local>
2023-04-19 12:35:18 +0800

    NestJS

--- AD/dash-be/controller/user-controller.ts
+++
@@ -1,7 +0,0 @@
-import User from "../model/Job";
-import express from "express";
-
-const userRouter = express.Router();
-
-console.log("user router connected");
-export default userRouter;
--- BE/service/job-services.ts
+++
@@ -1,7 +0,0 @@
-// import Job from "../model/Job";
-
-// export const addJobsList = async (job) => {
-//   return await Job.save({}).then((res) => {
-//     return res;
-//   });
-// };
