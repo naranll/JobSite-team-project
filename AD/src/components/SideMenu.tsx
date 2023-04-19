cf9a0b6cf657080a233aed206ddc015cd64f21f9
Naraa <naraa_togy@yahoo.com>
2023-04-20 07:43:21 +0800

    unfinished currentPage state

--- AD/src/components/SideMenu.tsx
+++ AD/src/components/SideMenu.tsx
@@ -1,27 +1,81 @@
 import Link from "next/link";
 import * as Logos from "./icons/Logos";
+import { useEffect, useState } from "react";
+
+type menuPageType = {
+  page: string;
+  url: string;
+  logo: JSX.Element;
+};
+
+const menuPages: Array<menuPageType> = [
+  {
+    page: "Home",
+    url: "/",
+    logo: <Logos.HomeLogo />,
+  },
+  {
+    page: "Jobs",
+    url: "/jobs",
+    logo: <Logos.JobLogo />,
+  },
+  {
+    page: "Users",
+    url: "/users",
+    logo: <Logos.UserLogo />,
+  },
+  {
+    page: "Applications",
+    url: "/applications",
+    logo: <Logos.ApplicationLogo />,
+  },
+  {
+    page: "Admin",
+    url: "/admin",
+    logo: <Logos.AdminLogo />,
+  },
+];
 
 export default function SideMenu(): JSX.Element {
-  const navlinkStyle = "flex items-center gap-[15px] text-xl font-bold mb-4 hover:underline hover:decoration-solid active:bg-white active:text-[#9F69B8]";
+  const [currentPage, setCurrentPage] = useState("/");
+
+  useEffect(() => {
+    if (localStorage.getItem("pageValue")) {
+      setCurrentPage(
+        menuPages.find(
+          (menuPage) => menuPage.page === localStorage.getItem("pageValue")
+        )
+      );
+    }
+  }, []);
+
+  function currentPageHandler(page: string) {
+    setCurrentPage(page);
+    localStorage.setItem("pageValue", page);
+  }
+
+  const navlinkStyle =
+    "flex items-center gap-[15px] text-xl font-bold mb-4 hover:underline hover:decoration-solid active:bg-white active:text-[#9F69B8]";
+
   return (
-    <div className="w-1/5 h-screen py-9 sticky top-0 text-white bg-gradient-to-b from-[#9F69B8] to-[#4D8BCC]"> 
-      <h1 className="w-[227px] m-auto mb-[52px] flex items-center gap-[13px] text-2xl font-black"><Logos.MenuLogo/>Dashboard</h1>
+    <div className="w-1/5 h-screen py-9 sticky top-0 text-white bg-gradient-to-b from-[#9F69B8] to-[#4D8BCC]">
+      <h1 className="w-[227px] m-auto mb-[52px] flex items-center gap-[13px] text-2xl font-black">
+        <Logos.MenuLogo />
+        Dashboard
+      </h1>
       <ul className="w-[227px] h-[500px] m-auto">
-        <Link href="/">
-          <li className={navlinkStyle}><Logos.HomeLogo/>Home</li>
-        </Link>
-        <Link href="/jobs">
-          <li className={navlinkStyle}><Logos.JobLogo/>Jobs</li>
-        </Link>
-        <Link href="/users">
-          <li className={navlinkStyle}><Logos.UserLogo/>Users</li>
-        </Link>
-        <Link href="/applications" >
-          <li className={navlinkStyle}><Logos.ApplicationLogo/>Applications</li>
-        </Link>
-        <Link href="/admin">
-          <li className={navlinkStyle}><Logos.AdminLogo/>Admin</li>
-        </Link>
+        {menuPages.map((menuPage: menuPageType, i: number) => (
+          <Link
+            key={i}
+            href={menuPage.url}
+            onClick={() => currentPageHandler(menuPage.page)}
+          >
+            <li className={navlinkStyle}>
+              {menuPage.logo}
+              {menuPage.page}
+            </li>
+          </Link>
+        ))}
       </ul>
     </div>
   );
