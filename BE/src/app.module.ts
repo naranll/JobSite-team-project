f87bebe0134768838499123c62effcdc60977035
Morning <morning@1-11.local>
2023-04-25 11:32:25 +0800

    single-job-apply

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { jobModule } from './job/job.module';
+import { applicationModule } from './application/application.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { jobModule } from './job/job.module';
     MongooseModule.forRoot(`${process.env.MONGO_DB_CONNECT}`),
     UserModule,
     jobModule,
+    applicationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- BE/src/application/application.controller.ts
+++ BE/src/application/application.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApplicationService } from './application.service';
-import { AppList } from './application.schema';
+import { Application } from './application.schema';
+import { ApplicationDto } from './application.dto';
 
-@Controller(`application`)
+@Controller('application')
 export class ApplicationController {
   constructor(private readonly applicationService: ApplicationService) {}
   @Get('all')
-  findAll(): Promise<AppList[]> {
+  findAll(): Promise<Application[]> {
     return this.applicationService.findAll();
+  }
+
+  @Post('add')
+  addAppList(@Body() body: ApplicationDto): Promise<Application> {
+    console.log('req body:', body);
+    return this.applicationService.addApp(body);
   }
 }
