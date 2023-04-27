72ff2ad36d39bbfcd907fd62e0f91156f9568b25
Morning <morning@1-9.local>
2023-04-27 12:46:24 +0800

    added getAppliedJobsById service

--- BE/src/application/application.controller.ts
+++ BE/src/application/application.controller.ts
@@ -1,24 +1,25 @@
-import {Body, Controller, Get, Param, Post} from "@nestjs/common";
-import {ApplicationService} from "./application.service";
-import {Application} from "./application.schema";
-import {ApplicationDto} from "./application.dto";
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { ApplicationService } from './application.service';
+import { Application } from './application.schema';
+import { ApplicationDto } from './application.dto';
+import { Job } from 'src/job/job.schema';
 
-@Controller("application")
+@Controller('application')
 export class ApplicationController {
   constructor(private readonly applicationService: ApplicationService) {}
-  @Get("all")
+  @Get('all')
   findAll(): Promise<Application[]> {
     return this.applicationService.findAll();
   }
 
-  @Post("add")
+  @Post('add')
   addAppList(@Body() body: ApplicationDto): Promise<Application> {
-    console.log("req body:", body);
+    console.log('req body:', body);
     return this.applicationService.addApp(body);
   }
 
-  @Get("/:id")
-  getAppliadJobsByUserId(@Param("id") userId: string) {
+  @Get('/:id')
+  getAppliadJobsByUserId(@Param('id') userId: string): Promise<void> {
     return this.applicationService.getAppliedJobsByUserId(userId);
   }
 }
--- BE/src/application/application.service.ts
+++ BE/src/application/application.service.ts
@@ -3,7 +3,7 @@ import { Connection, Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { Application } from './application.schema';
 import { ApplicationDto } from './application.dto';
-// import { Job } from "src/job/job.schema";
+import { Job } from 'src/job/job.schema';
 
 @Injectable()
 export class ApplicationService {
@@ -23,11 +23,11 @@ export class ApplicationService {
   async getAppliedJobsByUserId(userId: string): Promise<void> {
     const applications = await this.applicationModel
       .find({ userId })
-      .populate('jobId');
-    // const jobsId = applications.map((application) => application.jobId);
-    // console.log("applied jobsId", jobsId);
-    console.log("user's applications", applications);
+      .populate('jobId')
+      .select({ jobId: 1 });
+    console.log('applications', applications[0]);
+    // const appliedJobs = applications.map((application) => application.jobId);
+    // console.log('applied jobs', appliedJobs[0]);
+    // return appliedJobs;
   }
 }
-
-// .populate({path: "Job", options: {strictPopulate: false}})
