b80b3cba3842526c5cde0a1c99fde901fe7abd4c
Morning <morning@1-11.local>
2023-04-28 11:59:09 +0800

    fixed offcanvas menu

--- FE/src/components/Header.tsx
+++ FE/src/components/Header.tsx
@@ -1,68 +1,51 @@
 
-import {useState} from "react";
-import {useUserContext} from "../context/UserContext";
+// import {useUserContext} from "../../context/UserContext";
 
 import styles from "../styles/header.module.css";
 import Link from "next/link";
+import { Sidebar } from 'primereact/sidebar';
+import { Button } from 'primereact/button';
+import React, { useState } from "react";
+// import 'primeicons/primeicons.css';
+        
+
+
+
+        
 
 export default function Header(): JSX.Element {
-  const { user, handleLogout } = useUserContext();
-  const [offanvas, setOffcanvas] = useState(false);
-  // const [visible, setVisible] = useState(false)
+  // const {user, handleLogout} = useUserContext();
+  const [visible, setVisible] = useState<boolean>(false)
 
   return (
     <div className={styles.header}>
       <Link href={`/`}>
         <div className={styles.logo}>Logo</div>
       </Link>
-      <div className={styles.menu}>
-        {offanvas ? (
-          <div id="mySidenav" className="sidenav">
-            <div className={styles.filters}>
-              {user ? (
-                <div>
-                  <div>Hi! {user.firstName}</div>
-                  <button onClick={handleLogout}>Log out</button>
-                </div>
-              ) : (
-                <Link href={"/user/login"}>
-                  <div>LogIn</div>
-                </Link>
-              )}
-              <Link
-                href={`/user/appliedjobs`}
-                onClick={() => setOffcanvas(false)}
-              >
-                <div>Applied jobs</div>
-              </Link>
-              <Link
-                href={`/user/postedjobs`}
-                onClick={() => setOffcanvas(false)}
-              >
-                <div>Posted jobs</div>
-              </Link>
-            </div>
-          </div>
-        ) : null}
-      </div>
 
       <Link href={`/addjob`}>
         <span className={styles.post}>Post a Job</span>
       </Link>
 
-      <div id="mainBtn">
-        <span
-          className={`${offanvas ? styles.menuBtnHidden : styles.menuBtn} `}
-          onClick={() => {
-            setOffcanvas(true);
-          }}
-        >
-          &#9776;
-        </span>
-        <a className={styles.closebtn} onClick={() => setOffcanvas(false)}>
-          &times;
-        </a>
-      </div>
+      <div className="card flex justify-content-center ">
+      <Sidebar visible={visible} onHide={() => setVisible(false)}>
+      <div id="mySidenav" className="sidenav">
+            <div className={styles.filters}>
+              <Link
+                href={`/user/appliedjobs`}
+              >
+                <div className="AppliedJobs">Applied jobs</div>
+              </Link>
+              <Link
+                href={`/user/postedjobs`}
+              >
+                <div className="PostedJobs">Posted jobs</div>
+              </Link>
+            </div>
+          </div>
+        </Sidebar>
+        <Button className="pi pi-bars" onClick={() => setVisible(true)}/>
+</div>
     </div>
   );
 }
--- FE/src/pages/_app.tsx
+++ FE/src/pages/_app.tsx
@@ -5,6 +5,17 @@ import Layout from "@/components/Layout";
 import {UserContextProvider} from "../context/UserContext";
 
 
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";     
+    
+//core
+import "primereact/resources/primereact.min.css";  
+
+
+// import 'primeicons/primeicons.css';
+        
+        
+
 export default function App({Component, pageProps}: AppProps): JSX.Element {
   return (
     <UserContextProvider>
