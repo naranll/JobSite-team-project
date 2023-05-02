7487f8f2f8790146108545c9889f7e68f0765865
Morning <morning@1-10.local>
2023-05-02 11:18:49 +0800

    fe-applied-show-pending

--- FE/src/components/JobCard.tsx
+++ FE/src/components/JobCard.tsx
@@ -1,6 +1,7 @@
 import Card from "../styles/JobCard.module.scss";
 import { JobType } from "@/util/types";
 
+<<<<<<< HEAD
 export default function JobCard(props: JobType): JSX.Element {
   return (
     <div className={Card.wrap}>
@@ -8,6 +9,15 @@ export default function JobCard(props: JobType): JSX.Element {
         <h1 className={Card.cardTitle}>{props.title}</h1>
         <p className={Card.cardDisc}>{props.description}</p>
         <span className={Card.cardmoney}>{props.payment}</span>
+=======
+export default function JobCard(prop: JobType): JSX.Element {
+  return (
+    <div className={Card.wrap}>
+      <div className={Card.jobCard}>
+        <h1 className={Card.cardTitle}>{prop.title}</h1>
+        <p className={Card.cardDisc}>{prop.description}</p>
+        <span className={Card.cardmoney}>{prop.payment}</span>
+>>>>>>> 92ab35f (unfinished apllication state)
       </div>
     </div>
   );
