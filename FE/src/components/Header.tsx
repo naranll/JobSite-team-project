274e62b61974410e56becc8907b8c1556f6b770a
Naraa <morning@1-9.local>
2023-05-04 12:38:45 +0800

    fixed header style

--- FE/src/components/Header.tsx
+++ FE/src/components/Header.tsx
@@ -1,10 +1,8 @@
-import styles from "../styles/header.module.scss";
 import Link from "next/link";
 import {Sidebar} from "primereact/sidebar";
 import React, {useState} from "react";
 
 import "primeicons/primeicons.css";
-import {AiOutlineMenu} from "react-icons/ai";
 import {useUserContext} from "@/context/UserContext";
 
 export default function Header(): JSX.Element {
@@ -12,35 +10,38 @@ export default function Header(): JSX.Element {
   const [visible, setVisible] = useState<boolean>(false);
 
   return (
-    <div className={styles.header}>
-      <Link href={`/`}>
-        <div className={styles.logo}>Logo</div>
+    <div className="header center-element w-full h-[46px] md:h-[54px]">
+      <Link href={`/`} className="header-logo">
+        <div>Logo</div>
       </Link>
-
-      <Link href={`/addjob`}>
-        <span className="">Post a Job</span>
-      </Link>
-
-      <div className="card flex justify-content-center ">
-        <Sidebar visible={visible} onHide={() => setVisible(false)}>
-          <div id="mySidenav" className="sidenav">
-            <div className={styles.filters}>
-              <Link href={`/user/appliedjobs`}>
-                <div className="AppliedJobs">Applied jobs</div>
-              </Link>
-              <Link href={`../user/postedjobs`}>
-                <div className="PostedJobs">Posted jobs</div>
-              </Link>
-              {currentUser ? <div onClick={handleLogout}>Logout</div> : null}
-            </div>
-          </div>
-        </Sidebar>
-
-        <AiOutlineMenu
-          className={styles.offcanvasmenu}
-          onClick={() => setVisible(true)}
-        />
+      <div className="center-element gap-2">
+        <Link href={`/addjob`} className="button-style">
+          <span>Post a Job</span>
+        </Link>
+        {currentUser && (
+          <picture className="w-[40px]">
+            <img
+              src={currentUser.image}
+              alt="user"
+              onClick={() => setVisible(true)}
+            />
+          </picture>
+        )}
       </div>
+
+      <Sidebar visible={visible} onHide={() => setVisible(false)}>
+        <div>
+          <div>
+            <Link href={`/user/appliedjobs`}>
+              <div>Applied jobs</div>
+            </Link>
+            <Link href={`../user/postedjobs`}>
+              <div>Posted jobs</div>
+            </Link>
+            {currentUser ? <div onClick={handleLogout}>Logout</div> : null}
+          </div>
+        </div>
+      </Sidebar>
     </div>
   );
 }
