b647363f6a9f03e76c59e2cc922df7687b993518
Naraa <morning@1-9.local>
2023-05-05 10:26:07 +0800

    fixed jobcard style

--- FE/src/components/JobCard.tsx
+++ FE/src/components/JobCard.tsx
@@ -1,12 +1,17 @@
-import { JobType } from "@/util/types";
+import {JobType} from "@/util/types";
 
 export default function JobCard(props: JobType): JSX.Element {
   return (
-    <div>
-      <div className="jobCard">
-        <h1 className="cardTitle">{props.title}</h1>
-        <p className="cardDisc">{props.description}</p>
-        <span className="cardmoney">{props.payment}</span>
+    <div className="jobcard flex items-center gap-4 drop-shadow-sm">
+      <div className="jobcard-image invisible sm:visible sm:flex sm:w-[40px] sm:h-[40px] sm:rounded-full">
+        J
+      </div>
+      <div className="jobcard-details w-5/6">
+        <h1 className="jobcard-title">{props.title}</h1>
+        <p className="jobcard-description">
+          {props.description.slice(0, 150)}...
+        </p>
+        <span className="jobcard-wage">${props.payment}</span>
       </div>
     </div>
   );
