2f6d949d1a31d0690fccaeb0f796e919ab3cb7c9
Naraa <morning@1-9.local>
2023-05-05 10:27:27 +0800

    resolved merge conflict

--- FE/src/components/Header.tsx
+++ FE/src/components/Header.tsx
@@ -1,9 +1,19 @@
+<<<<<<< Updated upstream
 import Link from "next/link";
 import React, { useState } from "react";
 import { useUserContext } from "@/context/UserContext";
 import "primeicons/primeicons.css";
 import { Sidebar } from "primereact/sidebar";
 import { useRouter } from "next/router";
+=======
+import styles from "../styles/header.module.scss";
+import Link from "next/link";
+import {Sidebar} from "primereact/sidebar";
+import React, {useState} from "react";
+
+import "primeicons/primeicons.css";
+import {AiOutlineMenu} from "react-icons/ai";
+>>>>>>> Stashed changes
 
 export default function Header(): JSX.Element {
   const { currentUser, handleLogout } = useUserContext();
@@ -15,6 +25,7 @@ export default function Header(): JSX.Element {
       <Link href={`/`} className="header-logo">
         <div>Logo</div>
       </Link>
+<<<<<<< Updated upstream
       <div className="center-element gap-2">
         {!currentUser && (
           <Link
@@ -36,6 +47,31 @@ export default function Header(): JSX.Element {
             />
           </picture>
         )}
+=======
+
+      <Link href={`/addjob`}>
+        <span className="">Post a Job</span>
+      </Link>
+
+      <div className="card flex justify-content-center ">
+        <Sidebar visible={visible} onHide={() => setVisible(false)}>
+          <div id="mySidenav" className="sidenav">
+            <div className={styles.filters}>
+              <Link href={`/user/appliedjobs`}>
+                <div className="AppliedJobs">Applied jobs</div>
+              </Link>
+              <Link href={`../user/postedjobs`}>
+                <div className="PostedJobs">Posted jobs</div>
+              </Link>
+            </div>
+          </div>
+        </Sidebar>
+
+        <AiOutlineMenu
+          className={styles.offcanvasmenu}
+          onClick={() => setVisible(true)}
+        />
+>>>>>>> Stashed changes
       </div>
 
       <Sidebar
