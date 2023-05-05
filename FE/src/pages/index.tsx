3f0bf77fbf5a2b24a436ae2a07b318fa3f223300
Naraa <morning@1-9.local>
2023-05-05 10:02:09 +0800

    fixed index joblist

--- FE/src/pages/index.tsx
+++ FE/src/pages/index.tsx
@@ -1,25 +1,25 @@
 import JobCard from "@/components/JobCard";
-import { JobType } from "@/util/types";
+import {JobType} from "@/util/types";
 import Link from "next/link";
 
-export default function Home(props: { jobs: JobType[] }): JSX.Element {
-  const { jobs } = props;
+export default function Home(props: {jobs: JobType[]}): JSX.Element {
+  const {jobs} = props;
   return (
-    <div className="home-page">
-      <h1>JOB BOARD</h1>
-      <div className="search">
-        <input placeholder="search" />
-        <button>search</button>
-      </div>
-      {jobs.map(
-        (job: JobType, index: number): JSX.Element => (
-          <div key={index}>
-            <Link href={`jobs/${job._id}`}>
+    <div className="home-page flex flex-col items-center gap-3">
+      <h1 className="home-title">JOB BOARD</h1>
+      <form className="searchbar h-[30px] w-4/6 lg:h-[40px] drop-shadow-sm">
+        <input type="search" placeholder="Enter search" className="w-5/6" />
+        <button className="w-1/6 hidden sm:block">Search</button>
+      </form>
+      <div className="home-joblist w-5/6">
+        {jobs.map(
+          (job: JobType, index: number): JSX.Element => (
+            <Link href={`jobs/${job._id}`} key={index}>
               <JobCard {...job} />
             </Link>
-          </div>
-        )
-      )}
+          )
+        )}
+      </div>
     </div>
   );
 }
