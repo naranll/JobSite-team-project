ccc6aa982f3d404ebb0627c6a052614bf9d76bec
Naraa <naraa_togy@yahoo.com>
2023-05-08 02:45:22 +0800

    added simple filter function

--- FE/src/components/Filter.tsx
+++ FE/src/components/Filter.tsx
@@ -1,19 +1,53 @@
+import { useState, useEffect } from "react";
+
+interface CategoryType {
+  value: string;
+}
+
+const categories: CategoryType[] = [
+  {
+    value: "all",
+  },
+  {
+    value: "developer",
+  },
+  {
+    value: "designer",
+  },
+];
+
 export default function Filter() {
+  const [currentCategory, setCurrentCategory] = useState<string>("all");
+
+  useEffect(() => {
+    if (localStorage.getItem("category")) {
+      const category: string | null = localStorage.getItem("category");
+      category && setCurrentCategory(category);
+    }
+  }, []);
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function changeHandler(e: any): void {
+    console.log("filter", e.currentTarget.value);
+    setCurrentCategory(e.currentTarget.value);
+    localStorage.setItem("category", e.currentTarget.value);
+  }
+
   return (
     <form className="filter-form">
       <p className="filter-form-title">Category</p>
-      <label>
-        <input type="radio" name="category" value="all" checked />
-        All jobs
-      </label>
-      <label>
-        <input type="radio" name="category" value="developer" />
-        Developer
-      </label>
-      <label>
-        <input type="radio" name="category" value="designer" />
-        Designer
-      </label>
+      {categories.map((category: CategoryType, i: number) => (
+        <label key={i}>
+          <input
+            type="radio"
+            name="category"
+            value={category.value}
+            onChange={changeHandler}
+            checked={currentCategory === category.value ? true : false}
+          />
+          {category.value.charAt(0).toUpperCase() + category.value.slice(1)}
+        </label>
+      ))}
       <hr />
     </form>
   );
