e39d7e4000a42a53350a1f302f1d4926a67dc93f
Naraa <naraa_togy@yahoo.com>
2023-05-08 01:28:15 +0800

    finished filter component skeleton

--- FE/src/components/Filter.tsx
+++ FE/src/components/Filter.tsx
@@ -1,3 +1,20 @@
 export default function Filter() {
-  return <form>Filter</form>;
+  return (
+    <form className="filter-form">
+      <p className="filter-form-title">Category</p>
+      <label>
+        <input type="radio" name="category" value="all" checked />
+        All jobs
+      </label>
+      <label>
+        <input type="radio" name="category" value="developer" />
+        Developer
+      </label>
+      <label>
+        <input type="radio" name="category" value="designer" />
+        Designer
+      </label>
+      <hr />
+    </form>
+  );
 }
