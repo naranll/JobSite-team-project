b8606ddb7af19cb9d858591b87ab44dfd3a70f61
Naraa <morning@1-9.local>
2023-05-08 10:42:32 +0800

    added filter select, fixed footer

--- FE/src/pages/index.tsx
+++ FE/src/pages/index.tsx
@@ -1,26 +1,35 @@
 import Filter from "@/components/Filter";
 import JobCard from "@/components/JobCard";
-import { JobType } from "@/util/types";
+import {JobType} from "@/util/types";
 import Link from "next/link";
+import {FaFilter} from "react-icons/fa";
 
-export default function Home(props: { jobs: JobType[] }): JSX.Element {
-  const { jobs } = props;
+export default function Home(props: {jobs: JobType[]}): JSX.Element {
+  const {jobs} = props;
   return (
-    <div className="home-page flex flex-col items-center gap-3">
+    <div className="home-page flex flex-col items-center gap-3 overflow-y-scroll">
       <h1 className="home-title">JOB BOARD</h1>
-      <form className="searchbar h-[30px] w-4/6 lg:h-[40px]">
+      <form className="searchbar w-4/6 h-[32px] lg:h-[40px]">
         <input
           type="search"
           placeholder="Enter search"
           className="w-full sm:w-5/6"
         />
-        <button className="hidden sm:block sm:w-1/6">Search</button>
-        <div>filter</div>
+        <button className="hidden lg:block lg:w-1/6">Search</button>
+
+        <div className="home-filter-btn p-2 center-element lg:hidden">
+          <FaFilter />
+          <select>
+            <option>All</option>
+            <option>Developer</option>
+            <option>Designer</option>
+          </select>
+        </div>
       </form>
-      <div className="w-full lg:container lg:flex lg:gap-5">
-        {/* <div className="home-filter hidden lg:block lg:w-1/5 lg:h-[360px] lg:p-4 shadow"> */}
-        <Filter />
-        {/* </div> */}
+      <div className="w-full h-[32px] lg:container lg:flex lg:gap-5">
+        <div className="home-filter hidden lg:block lg:w-1/5 lg:h-[360px] lg:p-4 shadow">
+          <Filter />
+        </div>
         <div className="home-joblist mx-auto w-5/6 lg:w-4/5">
           {jobs.map(
             (job: JobType, index: number): JSX.Element => (
