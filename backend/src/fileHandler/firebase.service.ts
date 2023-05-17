1b51420b2d8cb4c3395001887f77a3863296c427
balja0803 <balja0803@gmail.com>
2023-05-17 12:52:15 +0800

    fixed firebase config

--- backend/src/fileHandler/firebase.service.ts
+++ backend/src/fileHandler/firebase.service.ts
@@ -1,10 +1,13 @@
 import * as admin from 'firebase-admin';
+import { ServiceAccount } from 'firebase-admin';
+
+import * as serviceAccount from '../firebase-admin-sdk.json';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const serviceAccount = require('../firebase-admin-sdk.json');
+// const serviceAccount = require('../firebase-admin-sdk.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
   storageBucket: 'gs://jobsite-385401.appspot.com',
 });
 
