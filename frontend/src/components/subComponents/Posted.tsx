4c3529306f0b56b6eec6d577fbd954b3ecddc3d8
Naraa <morning@1-9.local>
2023-05-17 12:10:35 +0800

    displayed user posted jobs

--- frontend/src/components/subComponents/Posted.tsx
+++ frontend/src/components/subComponents/Posted.tsx
@@ -1,24 +1,44 @@
-// import { useUserContext } from "@/context/UserContext";
-// import { JobType } from "@/util/types";
-// import axios from "axios";
+import {useUserContext} from "@/context/UserContext";
+import Cookies from "js-cookie";
+import {JobType} from "@/util/types";
 // import { useRouter } from "next/router";
-// import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
+import JobCard from "../JobCard";
 
 export default function Posted(): JSX.Element {
-  // const [jobs, setJobs] = useState<JobType[]>();
-  // const { currentUser, token } = useUserContext();
+  const [postedJobs, setPostedJobs] = useState<JobType[]>();
+  const {currentUser} = useUserContext();
   // const router = useRouter();
 
-  // useEffect(() => {
-  //   const fetchJobs = async (id: string | undefined) => {
-  //     console.log("user id :", id);
-  //     const result = axios.get(
-  //       `${process.env.NEXT_PUBLIC_JOBSITE_HOST}/job/posted/${currentUser?._id}`
-  //     );
-  //     console.log("job result :", result);
-  //   };
-  //   fetchJobs(currentUser?._id);
-  // }, []);
+  useEffect(() => {
+    console.log("user id", currentUser);
+    try {
+      const bearer = "Bearer " + Cookies.get("token");
 
-  return <div>Posted Jobs</div>;
+      const getPostedJobs = async () => {
+        const result = await fetch(
+          `${process.env.NEXT_PUBLIC_JOBSITE_HOST}/job/posted/${currentUser?._id}`,
+          {headers: {Authorization: bearer}}
+        ).then((res) => res.json());
+        console.log(result);
+        setPostedJobs(result);
+      };
+      getPostedJobs();
+    } catch (error) {}
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="">
+      {postedJobs?.map((job: JobType, i: number) => {
+        // const jobApplicants = filterJobApplicant(job._id);
+        console.log("posted jobs");
+        return (
+          <div key={i} className="postedjobs-jobcard">
+            <JobCard {...job} />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
