5f29bc4308385ae374d929e3afa58f8942e4f453
Naraa <morning@1-9.local>
2023-05-17 12:48:07 +0800

    tried display applicants on postedjobs

--- frontend/src/components/subComponents/Posted.tsx
+++ frontend/src/components/subComponents/Posted.tsx
@@ -4,17 +4,16 @@ import {JobType} from "@/util/types";
 // import { useRouter } from "next/router";
 import {useEffect, useState} from "react";
 import JobCard from "../JobCard";
+import {ApplicationType} from "@/util/types";
 
 export default function Posted(): JSX.Element {
   const [postedJobs, setPostedJobs] = useState<JobType[]>();
   const {currentUser} = useUserContext();
   // const router = useRouter();
+  const bearer = "Bearer " + Cookies.get("token");
 
   useEffect(() => {
-    console.log("user id", currentUser);
     try {
-      const bearer = "Bearer " + Cookies.get("token");
-
       const getPostedJobs = async () => {
         const result = await fetch(
           `${process.env.NEXT_PUBLIC_JOBSITE_HOST}/job/posted/${currentUser?._id}`,
@@ -28,14 +27,27 @@ export default function Posted(): JSX.Element {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  async function getJobApplicants(jobId: string | undefined) {
+    const result = await fetch(
+      `${process.env.NEXT_PUBLIC_JOBSITE_HOST}/application/applicants/${jobId}`,
+      {headers: {Authorization: bearer}}
+    ).then((res) => res.json());
+    console.log("applicants", result);
+    return result;
+  }
+
   return (
-    <div className="">
+    <div className="postedjobs-list">
       {postedJobs?.map((job: JobType, i: number) => {
         // const jobApplicants = filterJobApplicant(job._id);
-        console.log("posted jobs");
+        const jobApplicants = getJobApplicants(job._id);
+        console.log("posted job applicants", jobApplicants);
         return (
           <div key={i} className="postedjobs-jobcard">
             <JobCard {...job} />
+            <div className="postedjobs-applicants-count">
+              {jobApplicants.length()}
+            </div>
           </div>
         );
       })}
