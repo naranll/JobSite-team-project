16d66ab0b97c8660dd9e527d2766a092bb7d9870
Naraa <morning@1-9.local>
2023-05-18 10:59:09 +0800

    displayed user posted jobs

--- frontend/src/components/subComponents/Posted.tsx
+++ frontend/src/components/subComponents/Posted.tsx
@@ -1,15 +1,12 @@
 import {useUserContext} from "@/context/UserContext";
 import Cookies from "js-cookie";
 import {JobType} from "@/util/types";
-// import { useRouter } from "next/router";
 import {useEffect, useState} from "react";
 import JobCard from "../JobCard";
-import {ApplicationType} from "@/util/types";
 
 export default function Posted(): JSX.Element {
   const [postedJobs, setPostedJobs] = useState<JobType[]>();
   const {currentUser} = useUserContext();
-  // const router = useRouter();
   const bearer = "Bearer " + Cookies.get("token");
 
   useEffect(() => {
@@ -19,7 +16,7 @@ export default function Posted(): JSX.Element {
           `${process.env.NEXT_PUBLIC_JOBSITE_HOST}/job/posted/${currentUser?._id}`,
           {headers: {Authorization: bearer}}
         ).then((res) => res.json());
-        console.log(result);
+        console.log("fetch result", result);
         setPostedJobs(result);
       };
       getPostedJobs();
@@ -27,30 +24,23 @@ export default function Posted(): JSX.Element {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  async function getJobApplicants(jobId: string | undefined) {
-    const result = await fetch(
-      `${process.env.NEXT_PUBLIC_JOBSITE_HOST}/application/applicants/${jobId}`,
-      {headers: {Authorization: bearer}}
-    ).then((res) => res.json());
-    console.log("applicants", result);
-    return result;
-  }
+  // async function getJobApplicants(jobId: string | undefined) {
+  //   const result = await fetch(
+  //     `${process.env.NEXT_PUBLIC_JOBSITE_HOST}/application/applicants/${jobId}`,
+  //     {headers: {Authorization: bearer}}
+  //   ).then((res) => res.json());
+  //   console.log("applicants", result);
+  //   return [result];
+  // }
 
   return (
     <div className="postedjobs-list">
-      {postedJobs?.map((job: JobType, i: number) => {
-        // const jobApplicants = filterJobApplicant(job._id);
-        const jobApplicants = getJobApplicants(job._id);
-        console.log("posted job applicants", jobApplicants);
-        return (
-          <div key={i} className="postedjobs-jobcard">
-            <JobCard {...job} />
-            <div className="postedjobs-applicants-count">
-              {/* {jobApplicants.length()} */}
-            </div>
-          </div>
-        );
-      })}
+      {postedJobs?.map((job: JobType, i: number) => (
+        <div key={i} className="postedjobs-jobcard">
+          <JobCard {...job} />
+          <div className="postedjobs-applicants-count">{}</div>
+        </div>
+      ))}
     </div>
   );
 }
