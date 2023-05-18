8b9a82bc8b58cba023b35a87a0510c9b878c6bd5
Morning <morning@1-11.local>
2023-05-18 14:05:26 +0800

    fixed commit delete

--- frontend/src/pages/jobs/[id].tsx
+++ frontend/src/pages/jobs/[id].tsx
@@ -6,7 +6,6 @@ import {useState, useEffect} from "react";
 import SuccessModal from "@/components/SuccessModal";
 import {useRouter} from "next/router";
 import Cookies from "js-cookie";
-// import { getStaticPaths } from '../user/[id]/appliedjobs';
 import moment from "moment";
 
 export default function Job({data: job}: {data: JobType}): JSX.Element {
