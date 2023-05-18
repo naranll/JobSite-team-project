57551fbe4b575cc0d56a02e3b6aa29ef888376a7
Morning <morning@1-11.local>
2023-05-18 13:15:28 +0800

    display user applied jobs

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -23,7 +23,7 @@ export default function Login(): JSX.Element {
 
   function googleLogin() {
     axios
-      .get(`${process.env.NEXT_PUBLIC_JOBSITE_HOST}/google-login`)
+      .get(`${process.env.NEXT_PUBLIC_JOBSITE_HOST}google-login`)
       .then((res) => router.push(res.data));
   }
 
@@ -37,7 +37,7 @@ export default function Login(): JSX.Element {
       password: target.password.value,
     };
     axios
-      .post(`${process.env.NEXT_PUBLIC_JOBSITE_HOST}/user/login`, userLogin)
+      .post(`${process.env.NEXT_PUBLIC_JOBSITE_HOST}user/login`, userLogin)
       .then((res) => {
         if (res.status === 201) {
           const decoded: MyJwtPayload = jwtDecode(res.data.token);
