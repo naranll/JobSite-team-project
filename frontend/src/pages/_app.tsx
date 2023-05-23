9e81a5e62eb32cc06568b918bcaf3b7cb0a6e118
Дамбийням У <dambii5655@gmail.com>
2023-05-23 22:19:00 +0800

    fixed loader component name

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import "../styles/globals.scss";
-import type {AppProps} from "next/app";
+import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
-import {UserContextProvider} from "../context/UserContext";
+import { UserContextProvider } from "../context/UserContext";
 //theme
 import "primereact/resources/themes/lara-light-indigo/theme.css";
 //core
 import "primereact/resources/primereact.min.css";
-import {ChakraProvider} from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import Loader from "@/components/loader";
 // import 'primeicons/primeicons.css';
 
-export default function App({Component, pageProps}: AppProps): JSX.Element {
+export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ChakraProvider>
       <Loader>
